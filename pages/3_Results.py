0ba9cbf287006ba80dbff7fb39212664173caf0e
Dana @ Hive <dana.redena@ourhivehealth.com>
2026-01-08 09:46:14 +0100

    Calculate score

--- pages/3_Results.py
+++ pages/3_Results.py
@@ -6,7 +6,14 @@ import streamlit as st
 from utils.navigation import add_navigation_buttons
 from utils.config import SESSION_TECHNICAL_RESPONSES, SESSION_ORGANIZATIONAL_RESPONSES
 
-
+def calculate_score(organizational_questions):
+    score = 0
+    for response in organizational_questions:
+        answer_options = response.answer_options
+        answer_key = response.user_response
+        answer_value = next((pair[1] for pair in answer_options if pair[0] == answer_key), None)
+        score += answer_value
+    return score
 
 def main():
     st.title("Assessment Results")
@@ -19,7 +26,18 @@ def main():
 
     else:
         #Calculate the organizational score
+        organizational_questions = st.session_state[SESSION_ORGANIZATIONAL_RESPONSES].questions
+        technical_questions = st.session_state[SESSION_TECHNICAL_RESPONSES].questions
+
         st.write(st.session_state[SESSION_ORGANIZATIONAL_RESPONSES])
+
+        organizational_score = calculate_score(organizational_questions)
+        technical_score = calculate_score(technical_questions)
+
+        # TODO: Add a mechanism to store negative values
+
+        st.write(f"ORGANIZATIONAL SCORE: {organizational_score}")
+        st.write(f"TECHNICAL SCORE: {technical_score}")
 
     # Add navigation buttons
     st.markdown("---")
